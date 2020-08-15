d12b920160bf5087a645bdd1be5dbc91f094a5f0
Yozdel-Alanis <yox009@gmail.com>
2020-08-14 22:18:44 -0500

    Ya funcionan los poderes

---
+++ Assets/Scripts/MovementP.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class MovementP : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float movementVelocity = 10f;
+
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.01f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+
+            controller.Move(direction * movementVelocity * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    public float rayCatsDistance = 1f;
+
+    Rigidbody rb;
+
+    Vector3 input;
+    Camera cachedCamera;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        cachedCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        input.x = x;
+        input.z = y;
+
+        Ray ray = cachedCamera.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit, rayCatsDistance))
+        {
+            Debug.Log("Ray hitted with: " + hit.collider.gameObject.name);
+        }
+        Debug.DrawRay(ray.origin, ray.direction * rayCatsDistance, Color.red);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 velocity = input.normalized * speed;
+        rb.velocity = velocity;
+    }
+
+    private int GetIn()
+    {
+        int n = 1;
+        return n;
+    }
+}
